f9c1d532c751841ee27f12829af649d4688d13f2
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-09-29 16:24:14 +0200

    ajouter l'interace de repository pattern

--- Crypto Wallet Simulator/src/com/cryptoRepository/GenericRepository.java
+++ Crypto Wallet Simulator/src/com/cryptoRepository/GenericRepository.java
@@ -3,9 +3,9 @@ package com.cryptoRepository;
 import java.util.List;
 
 public interface GenericRepository<T, ID> {
-    void save(T entity);
-    T findById(ID id);
-    List<T> findAll();
-    void update(T entity);
-    void delete(ID id);
+ void save(T entity);
+ T findById(ID id);
+ List<T> findAll();
+ void update(T entity);
+ void delete(ID id);
 }
