efaa88f9892e353ca7481d5f39e35eadcbac86ba
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-09-30 10:37:47 +0200

    CWJ-23 Feat : ajoute generic class inmplment genirrec interface

--- Crypto Wallet Simulator/src/com/cryptoRepository/GenericRepositoryImpl.java
+++ Crypto Wallet Simulator/src/com/cryptoRepository/GenericRepositoryImpl.java
@@ -3,10 +3,14 @@ package com.cryptoRepository;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.logging.Logger;
+
+import com.util.AppLogger;
 
 public abstract class GenericRepositoryImpl<T, ID> implements GenericRepository<T, ID> {
     protected Connection connection;
     protected String tableName;
+    protected static final Logger logger = AppLogger.getLogger(GenericRepositoryImpl.class.getName());
 
     public GenericRepositoryImpl(Connection connection, String tableName) {
         this.connection = connection;
@@ -15,27 +19,13 @@ public abstract class GenericRepositoryImpl<T, ID> implements GenericRepository<
 
     @Override
     public void delete(ID id) {
-        try {
-            PreparedStatement ps = connection.prepareStatement(
-                "DELETE FROM " + tableName + " WHERE id = ?"
-            );
+        String sql = "DELETE FROM " + tableName + " WHERE id = ?";
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
             ps.setObject(1, id);
             ps.executeUpdate();
-            System.out.println("Entity deleted successfully!");
+            logger.info("Entity deleted successfully: " + id);
         } catch (SQLException e) {
-            e.printStackTrace();
+            logger.severe("Error deleting entity: " + e.getMessage());
         }
     }
-
-    @Override
-    public abstract void save(T entity);
-
-    @Override
-    public abstract T findById(ID id);
-
-    @Override
-    public abstract void update(T entity);
-
-    @Override
-    public abstract java.util.List<T> findAll();
 }
